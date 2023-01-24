ce2099c2519ae0dbfa2da99c82f4b99016de3259
Ian_Kratko <ian.kratko@itechart-group.com>
2023-01-24 14:38:41 +0200

    unit tests

--- CourseProject/Program.cs
+++ CourseProject/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace CourseProject
+﻿using System.Linq;
+
+namespace CourseProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            MainMenuHelper.DisplayMainScreen();
+            //MainMenuHelper.DisplayMainScreen();
         }
     }
 }
